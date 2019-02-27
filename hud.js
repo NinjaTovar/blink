a824f313c481f8e10d02d8244d5331fbb664fb1a
Andrew Kim <andrewkim787@gmail.com>
2019-02-26 22:31:07 -0800

    camera overhaul

--- hud.js
+++ hud.js
@@ -20,22 +20,20 @@ class Hud {
 class HealthBar {
 	constructor(game) {
 		this.game = game;
-		this.ctx = this.game.ctx;
 		this.camera = this.game.camera;
 		this.blink = this.game.blink;
 		this.health = this.blink.health;
 	}
 	update() {
 		this.health = this.blink.health;
-		this.dx = this.game.blink.x;
+		this.dx = -this.camera.x + 100;
+		this.dy = -this.camera.y + 100;
 
 	}
 	draw(ctx) {
-		// ctx.fillStyle = "#FF0000";
-		// ctx.fillRect(this.dx, 50, (this.health / 100) * 40, 25);
 
 		ctx.beginPath();
-		ctx.rect(this.dx, 20, 40 * (this.health / 100), 20);
+		ctx.rect(this.dx, this.dy, 40 * (this.health / 100), 20);
 		if (this.health > 630) {
 			ctx.fillStyle = "green"
 		} else if (this.health > 370) {
